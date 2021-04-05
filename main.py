491ae6dfe09e04124c81c784a137a778cca54098
Fawadul Haq <fawadulh@gmail.com>
2021-04-05 13:40:32 -0400

    add model

---
+++ main.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import warnings
+
+import tensorflow.keras as tf
+
+from sklearn.model_selection import train_test_split
+
+# Read data
+ratings_df = pd.read_csv("book-data/ratings.csv") 
+books_df = pd.read_csv("book-data/books.csv")
+ratings_df.head()
+
+# Print statistics
+print(ratings_df.shape)
+print(ratings_df.user_id.nunique())
+print(ratings_df.book_id.nunique())
+ratings_df.isna().sum()
+
+# SPlit data
+Xtrain, Xtest = train_test_split(ratings_df, test_size=0.2, random_state=1)
+print(f"Shape of train data: {Xtrain.shape}")
+print(f"Shape of test data: {Xtest.shape}")
+
+#Get the number of unique entities in books and users columns
+nbook_id = ratings_df.book_id.nunique()
+nuser_id = ratings_df.user_id.nunique()
+
+#Book input network
+input_books = tf.layers.Input(shape=[1])
+embed_books = tf.layers.Embedding(nbook_id + 1,15)(input_books)
+books_out = tf.layers.Flatten()(embed_books)
+
+#user input network
+input_users = tf.layers.Input(shape=[1])
+embed_users = tf.layers.Embedding(nuser_id + 1,15)(input_users)
+users_out = tf.layers.Flatten()(embed_users)
+
+conc_layer = tf.layers.Concatenate()([books_out, users_out])
+x = tf.layers.Dense(128, activation='relu')(conc_layer)
+x_out = x = tf.layers.Dense(1, activation='relu')(x)
+model = tf.Model([input_books, input_users], x_out)
+
+# Compiling network
+opt = tf.optimizers.Adam(learning_rate=0.001)
+model.compile(optimizer=opt, loss='mean_squared_error')
+model.summary()
+
+# Training and saving
+hist = model.fit([Xtrain.book_id, Xtrain.user_id], Xtrain.rating, 
+                 batch_size=64, 
+                 epochs=5, 
+                 verbose=1,
+                 validation_data=([Xtest.book_id, Xtest.user_id], Xtest.rating))
+
+
+train_loss = hist.history['loss']
+val_loss = hist.history['val_loss']
+plt.plot(train_loss, color='r', label='Train Loss')
+plt.plot(val_loss, color='b', label='Validation Loss')
+plt.title("Train and Validation Loss Curve")
+plt.legend()
+plt.show()
+
+#save the model
+model.save('model')
+
+#Making recommendations for user 100
+book_arr = np.array(b_id) #get all book IDs
+user = np.array([100 for i in range(len(b_id))])
+pred = model.predict([book_arr, user])
+
+pred = pred.reshape(-1) #reshape to single dimension
+pred_ids = (-pred).argsort()[0:5]
+
+books_df.iloc[pred_ids]
